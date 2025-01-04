d1a0e38e7b844195026d40fe7a9db18264a95e9c
SamiKolarkar <kolarkarsami@gmail.com>
2025-01-04 09:08:24 +0530

    Add files via upload

---
+++ 1 ifelse.py
@@ -0,0 +1,13 @@
+x=int(input("Enter value of x:"))
+
+if(x<=4):
+ print("ONE")
+ print("TWO")
+ print("THREE")
+ print("FOUR")
+else:
+    print("FIVE")
+    print("SIX")
+    print("SEVEN")
+    print("EIGHT")
+    
